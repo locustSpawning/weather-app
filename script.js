f4c784f00c8ef8af483fd3d04518c4e389534448
chels <chelsleonhardt@gmail.com>
2023-07-04 20:31:08 -0400

    User input getting data - will finish tomorrow!!

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+console.log('💧');
+
 var printDate;
 var date;
 var day;
 var month;
 var year;
-
-let locationSearch = document.getElementById('city-search').value;
 
 window.addEventListener('load', (event) => {
     //load date
@@ -18,15 +18,91 @@ window.addEventListener('load', (event) => {
     printDate.innerText = currentDate;
 });
 
-console.log('🌙');
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('#submit-btn');
+const error = document.querySelector('#error-msg');
+form.addEventListener('submit', handleSubmit);
+submitBtn.addEventListener('click', handleSubmit);
 
-fetch(
-    `https://api.weatherapi.com/v1/current.json?key=9728ba0a63ac4ca39e1154549230307&q=swedesboro`,
-    { mode: 'cors' }
-)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (response) {
-        console.log(response);
-    });
+function handleSubmit(e) {
+    e.preventDefault();
+    fetchWeather();
+}
+
+async function getWeatherData(location) {
+    const response = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=9728ba0a63ac4ca39e1154549230307&q=${location}`,
+        { mode: 'cors' }
+    );
+    if (response.status === 400) {
+        throwErrorMsg();
+    } else {
+        error.computedStyleMap.display = 'none';
+        const weatherData = await response.json();
+        const newData = processData(weatherData);
+        displayData(newData);
+        reset();
+    }
+}
+
+function throwErrorMsg() {
+    error.computedStyleMap.display = 'block';
+}
+
+function processData(weatherData) {
+    //grab all data to display on page
+    const myData = {
+        condition: weatherData.current.text,
+        feelsLike: {
+            f: Math.round(weatherData.current.feelslike_f),
+            c: Math.round(weatherData.current.feelslike_c),
+        },
+        currentTemp: {
+            f: Math.round(weatherData.current.temp_f),
+            c: Math.round(weatherData.current.temp_c),
+        },
+        wind: Math.round(weatherData.current.wind_mph),
+        humidity: weatherData.current.humidity,
+        location: weatherData.location.name.toUpperCase(),
+    };
+
+    //if in the US, add state
+    //if not, add ccountry
+    if (weatherData.location.country == 'United States of America') {
+        myData['region'] = weatherData.location.region.toUpperCase();
+    } else {
+        myData['region'] = weatherData.location.country.toUpperCase();
+    }
+
+    return myData;
+}
+
+function displayData(newData) {
+    const weatherInfo = document.getElementsByClassName('info');
+
+    document.querySelector('#condition').textContent = newData.condition;
+    document.querySelector(
+        '#location'
+    ).textContent = `${newData.location}, ${newData.region}`;
+    document.querySelector('#degrees'), (textContent = newData.currentTemp.f);
+    document.querySelector(
+        '#feels-like'
+    ).textContent = `Feels like ${newData.feelsLike.f}`;
+    document.querySelector(
+        '#wind-mph'
+    ).textContent = `Wind: ${newData.wind}mph`;
+    document.querySelector(
+        '#humidity'
+    ).textContent = `Humidity: ${newData.humidity}`;
+}
+
+function reset() {
+    form.reset();
+}
+
+// get location from user
+function fetchWeather() {
+    const input = document.querySelector('input[type="text"]');
+    const userLocation = input.value;
+    getWeatherData(userLocation);
+}
